e8be16da5b2e86ef613cfcae9f774ba8d27e9123
Volodymyr Borovets <vborovets0@gmail.com>
2023-09-12 08:49:41 +0300

    Add email instead of username authentication, add custom AuthTokenSerializer

--- user/views.py
+++ user/views.py
@@ -1,9 +1,10 @@
 from rest_framework import generics
+from rest_framework.authentication import TokenAuthentication
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.settings import api_settings
 
-from user.serializers import UserSerializer
+from user.serializers import UserSerializer, MyAuthTokenSerializer
 
 
 class CreateUserView(generics.CreateAPIView):
@@ -13,10 +14,12 @@ class CreateUserView(generics.CreateAPIView):
 
 class CreateTokenView(ObtainAuthToken):
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+    serializer_class = MyAuthTokenSerializer
 
 
 class ManageUserView(generics.RetrieveUpdateAPIView):
     serializer_class = UserSerializer
+    authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated, )
 
     def get_object(self):
